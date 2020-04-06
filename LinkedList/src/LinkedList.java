9b5e4cd4819b0537de053460b4e6531326063ac7
Neugaoyang <137542696@qq.com>
2020-04-06 13:47:33 +0800

    added linked list add methods

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -42,18 +42,15 @@ public class LinkedList<E> {
     public void add(int index, E e) {
         if (index < 0 || index > size)
             throw new IllegalArgumentException("index error");
-        if (index == 0) {
-            addFirst(e);
-        } else {
-            Node prev = dummyHead;
-            for (int i = 0; i < index; i++) {
-                prev = prev.next;
-            }
-            Node node = new Node(e);
-            node.next = prev.next;
-            prev.next = node;
-            size++;
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
+        Node node = new Node(e);
+        node.next = prev.next;
+        prev.next = node;
+        size++;
     }
 
     public void addFirst(E e) {
@@ -62,5 +59,56 @@ public class LinkedList<E> {
 
     public void addLast(E e) {
         add(size, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("index error");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("index error");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("Null");
+       return res.toString();
     }
 }
---
+++ LinkedList/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(0, 3);
+        linkedList.add(1, 4);
+        linkedList.add(2, 5);
+        linkedList.add(3, 6);
+        System.out.println(linkedList);
+    }
+}
