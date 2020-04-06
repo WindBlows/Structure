fec87320198d9f40ffb3dfe5435b24ee87c2fafb
Neugaoyang <137542696@qq.com>
2020-04-06 13:14:39 +0800

    linked list dummy head

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -23,11 +23,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead;
     private int size;
 
     public LinkedList() {
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -39,21 +39,14 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    public void addFirst(E e) {
-        Node node = new Node(e);
-        node.next = head;
-        head = node;
-        size++;
-    }
-
     public void add(int index, E e) {
         if (index < 0 || index > size)
             throw new IllegalArgumentException("index error");
         if (index == 0) {
             addFirst(e);
         } else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++) {
+            Node prev = dummyHead;
+            for (int i = 0; i < index; i++) {
                 prev = prev.next;
             }
             Node node = new Node(e);
@@ -61,6 +54,10 @@ public class LinkedList<E> {
             prev.next = node;
             size++;
         }
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
     }
 
     public void addLast(E e) {
